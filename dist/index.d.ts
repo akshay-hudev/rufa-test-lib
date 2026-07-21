8fcae2551167266e17fae19ae8783e650c5208db
Akshay H M <akshayhudevmanjunath@gmail.com>
2026-07-21 16:53:53 +0530

    Add controlled dead-code for project

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function formatCurrency(amount: number): string;
-export declare function slugify(text: string): string;
-export declare function truncateText(text: string, maxLength: number): string;
-export declare function parseDateRange(input: string): {
-    start: Date;
-    end: Date;
-};
-export declare function debounce<T extends (...args: any[]) => void>(fn: T, ms: number): T;
